7c6a5f10e8fad3c767eeb76a6cac892a3716759d
Miguel Magueijo <miguelmagueijo@hotmail.com>
2025-04-23 23:57:53 +0100

    expose package version in vite

--- WEB/vite.config.ts
+++ WEB/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from "@tailwindcss/vite";
+import { env } from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
+    plugins: [svelte(), tailwindcss()],
+    define: {
+        "__APP_VERSION__": JSON.stringify(env.npm_package_version),
+    }
 })
