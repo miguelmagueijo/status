8301587dc4eabc220961e8d49e0dae25fb47931c
Miguel Magueijo <miguelmagueijo@hotmail.com>
2025-04-25 00:58:52 +0100

    remove svelte logs of css unused selector

--- WEB/svelte.config.js
+++ WEB/svelte.config.js
@@ -1,7 +1,16 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    onwarn: (warning, handler) => {
+        const {code, _} = warning;
+
+        if (code === "css_unused_selector") {
+            return;
+        }
+
+        handler(warning);
+    }
 }
